78d0cddab47f9b597262b91297caf0ed2fbde93b
Eladio Mejias <eladioj6@gmail.com>
2021-08-29 17:36:29 -0400

    Adding search tables

---
+++ DemoADO/DemoSearch.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Title="Demo" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="DemoSearch.aspx.cs" Inherits="DemoADO.DemoSearch" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <br />
+
+    <h2>Search a Student</h2>
+
+    <div> 
+        
+       <table>  
+        <tr>  
+        <td>   
+           Buscar  
+            </td>  
+                <td>  
+                    <asp:TextBox ID="TextBox1" runat="server" CssClass="padding-form"></asp:TextBox>  
+                </td>  
+                <td>   
+                  <asp:Button ID="Button1" runat="server" Text="Buscar estudiante" CssClass="padding-form" onclick="Button1_Click" />  
+                </td>  
+            </tr>  
+        </table> 
+        
+        <table>
+            <tr>
+                <td>
+                    <p>
+                        <asp:Label ID="Label2" runat="server" Text="label"></asp:Label>  </p>
+
+                </td>
+            </tr>
+        </table>  
+
+        <asp:GridView ID="GridView1" runat="server"  BackColor="#DEBA84" BorderColor="#DEBA84" BorderStyle="None" BorderWidth="1px" 
+        CellPadding="3" CellSpacing="2" Height="90px" Width="260px">
+        <RowStyle BackColor="#FFF7E7" ForeColor="#8C4510" />
+        <FooterStyle BackColor="#F7DFB5" ForeColor="#8C4510" />
+        <PagerStyle ForeColor="#8C4510" HorizontalAlign="Center" />
+        <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#A55129" Font-Bold="True" ForeColor="White" /> 
+
+        </asp:GridView>
+
+     </div>  
+    
+</asp:Content>
---
+++ DemoADO/DemoSearch.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static DemoADO.connection;
+
+namespace DemoADO
+{
+    public partial class DemoSearch : System.Web.UI.Page
+    {
+
+        // Creando clase connection con sus respectivos metodos
+        connection conn = new connection();
+
+        public SqlConnection con;
+        public string constr;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            Label2.Visible = false;
+            //conn.openConnection();
+        }
+
+        // Segunda clase de conexion
+        public void connection2()
+        {
+            constr = ConfigurationManager.ConnectionStrings["connectionASPX"].ToString();
+            con = new SqlConnection(constr);
+            con.Open();
+
+        }
+
+        public void close()
+        {
+            con.Close();
+        }
+
+        private void rep_bind()
+        {
+            connection2();
+            string query = "select * from Person where Nombre = '" + TextBox1.Text + "'";
+            //SqlCommand cmd = new SqlCommand(query, conn.getConnection());
+            //cmd.CommandType = CommandType.Text;
+            SqlDataAdapter da = new SqlDataAdapter(query, con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            connection2();
+            string query = "select * from Person where Nombre = '" + TextBox1.Text.Trim() + "'";
+            SqlCommand com = new SqlCommand(query, con);
+
+            SqlDataReader dr;
+            dr = com.ExecuteReader();
+
+            if (dr.HasRows)
+            {
+                dr.Read();
+                rep_bind();
+                GridView1.Visible = true;
+                TextBox1.Text = "";
+                Label2.Text = "";
+            }
+            else
+            {
+                GridView1.Visible = false;
+                Label2.Visible = true;
+                if (string.IsNullOrEmpty(TextBox1.Text.ToString()))
+                {
+                    Label2.Text = "Ingreso un elemento vacio.";
+                }
+                else
+                {
+                  Label2.Text = "El termino " + TextBox1.Text.Trim() + " no se encuentra en la base de datos.";
+                }
+            }
+
+            con.Close();
+        }
+    }
+}
